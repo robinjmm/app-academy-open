401e7e38ba9a3710811b70a651519c95541005ae
J.M. Robin <johnmichaelrobin@gmail.com>
2022-05-17 13:12:11 +0800

    Add solution for Boolean exercise

---
+++ exercises/ex-03.js
@@ -0,0 +1,14 @@
+// Write 3 examples of expressions using each of the operations: !, &&, ||.
+// Print out the result of your expressions using console.log.
+
+console.log(!false);
+console.log(!true);
+console.log(!!true);
+
+console.log(true || false);
+console.log(false || true);
+console.log(true || true);
+
+console.log(true && true);
+console.log(false && true);
+console.log(true && false);
