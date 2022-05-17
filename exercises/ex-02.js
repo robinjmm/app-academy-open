66bf097fa977a707fc483d90a33c2b38202bd3e0
J.M. Robin <johnmichaelrobin@gmail.com>
2022-05-17 13:11:38 +0800

    Add solution for Numbers exercise

---
+++ exercises/ex-02.js
@@ -0,0 +1,8 @@
+// Write 5 examples of expressions using each of the operations: +, -, *, /, and %. 
+// Print out the result of your expressions using console.log. 
+
+console.log(1 + 2);
+console.log(10 - 5);
+console.log(5 * 4);
+console.log(120 / 6);
+console.log(50 % 2);
