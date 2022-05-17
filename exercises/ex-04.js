942a1c6cb004699a902a42613fb659c3c8e766a4
J.M. Robin <johnmichaelrobin@gmail.com>
2022-05-17 13:12:35 +0800

    Add solution for variables exercise

---
+++ exercises/ex-04.js
@@ -0,0 +1,19 @@
+// 1. Declare a variable called `firstName` and assign it your first name as a string:
+let firstName = "John Michael";
+
+// 2. Declare a variable called `lastName` and assign it your last name as a string:
+let lastName = "Robin";
+
+// 3. Declare a variable called `age`:
+let age;
+
+// 4. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+console.log(firstName, lastName, age);
+
+// 5. Assign the `age` variable to a number:
+age = 25;
+
+// 6. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+console.log(firstName, lastName, age);
