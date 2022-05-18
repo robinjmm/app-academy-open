05b43b35d456ee93dced056317b91e88e72c4c33
J.M. Robin <johnmichaelrobin@gmail.com>
2022-05-18 15:09:30 +0800

    Move exercises to proper folder

---
+++ 02_intro-to-functions/divide-by-three.js
@@ -0,0 +1,9 @@
+// Write a method divideByThree(num) that returns the passed in number argument divided by three.
+
+function divideByThree(num) {
+    return num / 3;
+}
+
+console.log(divideByThree(9)); // => 3.0
+console.log(divideByThree(15)); // => 5.0
+console.log(divideByThree(98)); // => 32.666666666666664
