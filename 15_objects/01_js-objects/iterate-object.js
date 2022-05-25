0245a892d43797da49c0201293433a0ead06f1cb
J.M. Robin <johnmichaelrobin@gmail.com>
2022-05-25 18:10:52 +0800

    Complete Javascript Objects exercises

---
+++ 15_objects/01_js-objects/iterate-object.js
@@ -0,0 +1,19 @@
+// Write a function printObject(obj) that prints out all key-value pairs of an object.
+// The format should be key - value.
+
+function printObject(obj) {
+    for (let key in obj) {
+        console.log(`${key} - ${obj[key]}`);
+    }
+}
+
+let bootcamp = {
+    name: "App Academy",
+    color: "Red",
+    population: 120
+};
+
+printObject(bootcamp); // prints
+  // name - App Academy
+  // color - Red
+  // population - 120
