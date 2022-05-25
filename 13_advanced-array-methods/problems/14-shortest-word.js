be93fc035364b922bd9c693d5998b7cddee4776b
J.M. Robin <johnmichaelrobin@gmail.com>
2022-05-25 13:27:04 +0800

    Complete Advanced Array Methods exercises

---
+++ 13_advanced-array-methods/problems/14-shortest-word.js
@@ -0,0 +1,37 @@
+/*
+Write a function `shortestWord` that accepts a sentence as an argument.
+The function should return the shortest word in the sentence. If there is a tie,
+return the word that appears later in the sentence.
+
+Solve this using Array's `forEach()`, `map()`, `filter()` **OR** `reduce()`
+methods.
+
+Examples:
+
+console.log(shortestWord('what a wonderful life'));     // 'a'
+console.log(shortestWord('the quick brown fox jumps')); // 'fox'
+console.log(shortestWord('do what you enjoy'));         // 'do'
+
+*/
+
+let shortestWord = function (sentence) {
+    let words = sentence.split(" ");
+
+    return words.reduce(function (acc, current) {
+        if (acc.length >= current.length) {
+            return current;
+        } else {
+            return acc;
+        }
+    })
+};
+
+// Your code here
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+try {
+    module.exports = shortestWord;
+} catch (e) {
+    module.exports = null;
+}
---
+++ 13_advanced-array-methods/problems/16-hipsterfy.js
@@ -0,0 +1,48 @@
+/*
+
+Write a function `hipsterfy(sentence)` that takes in a sentence string and
+returns the sentence where every word is missing it's last vowel.
+
+Solve this using Array's `forEach()`, `map()`, `filter()` **OR** `reduce()`
+methods.
+
+Examples:
+
+console.log(hipsterfy('When should everyone wake up?')); // 'Whn shold everyon wak p?'
+console.log(hipsterfy('get ready for our bootcamp')); // 'gt redy fr or bootcmp'
+console.log(hipsterfy('panthers are great animals')); // 'panthrs ar gret animls'
+
+*/
+
+let removeLastVowel = function (word) {
+    let vowels = "aeiou";
+
+    for (let i = word.length - 1; i >= 0; i--) {
+        if (vowels.includes(word[i])) {
+            return word.slice(0, i) + word.slice(i + 1);
+        }
+    }
+};
+
+let hipsterfy = function (sentence) {
+    let words = sentence.split(" ");
+
+    let newWords = words.map(function (word) {
+        return removeLastVowel(word);
+    })
+
+    return newWords.join(" ");
+};
+
+// alternative solution using Array.map
+// let hipsterfy = function(sentence) {
+//     return sentence.split(' ').map(removeLastVowel).join(' ');
+// };
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+try {
+    module.exports = hipsterfy;
+} catch (e) {
+    module.exports = null;
+}
