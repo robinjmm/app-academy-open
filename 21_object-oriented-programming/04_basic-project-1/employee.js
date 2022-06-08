a8b336f8eab8633ab199fdfa687037f93d23f8d3
J.M. Robin <johnmichaelrobin@gmail.com>
2022-06-08 12:04:19 +0800

    Improve solution for project

--- 21_object-oriented-programming/04_basic-project-1/employee.js
+++ 21_object-oriented-programming/04_basic-project-1/employee.js
@@ -1,43 +1,19 @@
 class Employee {
-    constructor(name, salary, title, boss) {
+    constructor(name, salary, title, manager) {
         this.name = name;
         this.title = title;
         this.salary = salary;
-        this.boss = boss;
 
-        if (boss !== undefined) {
-            boss.addEmployee(this);
-        }
-    }
-
-    salaries = [];
-
-    getSalaries(person) {
-        if (person.employees === undefined) {
-            return person.salary;
-        }
-
-        for (let i = 0; i < person.employees.length; i++) {
-            let employee = person.employees[i];
-            this.salaries.push(employee.salary)
-            this.getSalaries(employee);
-        }
-
-        return this.salaries;
-    }
-
-    bonus(multiplier) {
-        let result = this.getSalaries(this);
-        let total;
-
-        if (Array.isArray(result)) {
-            let reduced = result.reduce((prev, current) => prev + current);
-            total = reduced + this.salary;
+        if (manager === undefined) {
+            this.manager = null;
         } else {
-            total = result;
+            this.manager = manager;
+            this.manager.addEmployee(this);
         }
+    }
 
-        return total * multiplier;
+    calculateBonus(multiplier) {
+        return this.manager._totalSubSalary(this.manager) * multiplier;
     }
 }
 
--- 21_object-oriented-programming/04_basic-project-1/manager.js
+++ 21_object-oriented-programming/04_basic-project-1/manager.js
@@ -2,19 +2,33 @@ const Employee = require('./employee');
 
 class Manager extends Employee {
 
-    // Array to hold all employees under a certain manager
-    employees = [];
+    constructor(name, salary, title, manager) {
+        super(name, salary, title, manager);
+
+        this.employees = [];
+    }
 
     addEmployee(employee) {
         this.employees.push(employee);
     }
 
-    getEmployeeSalaries() {
-        if (!(this instanceof Manager)) {
-            console.log("Not a Manager!");
-        } else {
-            console.log("I'm a manager");
+    salaries = [];
+
+    _totalSubSalary(person = this) {
+        let totalSalary = 0;
+
+        for (let i = 0; i < person.employees.length; i++) {
+            let employee = person.employees[i];
+
+            if (employee instanceof Manager) {
+                totalSalary += employee.salary
+            } else {
+                totalSalary += employee.salary;
+                return totalSalary;
+            }
         }
+
+        return totalSalary;
     }
 }
 
